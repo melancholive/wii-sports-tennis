ccb32c6a85e6c02aee0dba585a46851ad72692ae
Si Yue Jiang <sj3834@nyu.edu>
2025-03-01 23:50:33 -0500

    text edit

--- SDLProject/main.cpp
+++ SDLProject/main.cpp
@@ -1,7 +1,7 @@
 /**
 * Author: Si Yue Jiang
 * Assignment: Pong Clone
-* Date due: 2025-02-15, 11:59pm
+* Date due: 2025-03-01, 11:59pm
 * I pledge that I have completed this assignment without
 * collaborating with anyone else, in conformance with the
 * NYU School of Engineering Policies and Procedures on
